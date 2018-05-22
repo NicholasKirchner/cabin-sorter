14502c35b4db20f2e7a280adb36ba70c6b9526ba
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-05-21 19:24:43 -0500

    production environment needs eager loads

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CabinSeparator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.eager_load_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
