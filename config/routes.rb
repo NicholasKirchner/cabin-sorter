e36191e67ca219abe8ef6a15fbb45db2f35a04c8
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-06-21 20:00:47 -0500

    routes for retreats

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   resources :assignments, only: [:new, :create]
+  resources :retreats
 end
