e4414eeb1e78a62c6e7c0c72ff54f599b5f2aa49
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-06-22 16:05:33 -0500

    annotate the models

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: campers
+#
+#  id         :bigint(8)        not null, primary key
+#  gender     :integer
+#  name       :string
+#  pin_hash   :string
+#  type       :integer
+#  retreat_id :integer
+#
+
 class Camper < ApplicationRecord
 
   belongs_to :retreat
--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: retreats
+#
+#  id             :bigint(8)        not null, primary key
+#  cabin_capacity :integer
+#  description    :string
+#  user_id        :integer
+#
+
 class Retreat < ApplicationRecord
 
   belongs_to :user
