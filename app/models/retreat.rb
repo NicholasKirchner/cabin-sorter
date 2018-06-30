a55f0589b6d6d0ba259893073a30dd8a4592eb4f
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-06-30 12:13:29 -0500

    fun with activerecord associations

--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -12,4 +12,9 @@ class Retreat < ApplicationRecord
 
   belongs_to :user
 
+  has_many :male_students,   -> { where(gender: 'M') }, class_name: 'Camper'
+  has_many :female_students, -> { where(gender: 'F') }, class_name: 'Camper'
+
+
+  
 end
