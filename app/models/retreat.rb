9957ddb38571d9e5388638f86514732d7015a7db
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-07-01 19:30:30 -0500

    bare bones form

--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -15,6 +15,12 @@ class Retreat < ApplicationRecord
   has_many :male_students,   -> { where(gender: 'M') }, class_name: 'Camper'
   has_many :female_students, -> { where(gender: 'F') }, class_name: 'Camper'
 
+  def male_student_list
+    male_students.collect(&:name).join("\n")
+  end
 
+  def female_student_list
+    female_students.collect(&:name).join("\n")
+  end
   
 end
