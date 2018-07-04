13010f2f1e6ff34a02745cfcf015196ae23e4bb3
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-07-04 16:06:22 -0500

    male/female student lists will be attributes

--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -19,8 +19,20 @@ class Retreat < ApplicationRecord
     male_students.collect(&:name).join("\n")
   end
 
+  def male_student_list=(a_string)
+    a_string.split("\n").each do |student|
+      self.male_students << Camper.new(type: 0, name: student, gender: 0)
+    end
+  end
+
   def female_student_list
     female_students.collect(&:name).join("\n")
   end
-  
+
+  def female_student_list=(a_string)
+    a_string.split("\n").each do |student|
+      self.female_students << Camper.new(type: 0, name: student, gender: 1)
+    end
+  end
+
 end
