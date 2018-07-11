256ca81e2dd9df90bef5db4d659a1ba65907f6c1
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-07-11 15:13:48 -0500

    update has_many relation to handle the enum

--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -12,8 +12,8 @@ class Retreat < ApplicationRecord
 
   belongs_to :user
 
-  has_many :male_students,   -> { where(gender: 'M') }, class_name: 'Camper'
-  has_many :female_students, -> { where(gender: 'F') }, class_name: 'Camper'
+  has_many :male_students,   -> { where(gender: :male) }, class_name: 'Camper'
+  has_many :female_students, -> { where(gender: :female) }, class_name: 'Camper'
 
   def male_student_list
     male_students.collect(&:name).join("\n")
