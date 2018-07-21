cbb4dbaf208cded8fbae73aabcf29e9f71a68c56
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2018-07-21 15:45:07 -0500

    work in progress: first React bits

---
+++ app/views/retreats/camper.jsx
@@ -0,0 +1,18 @@
+function Student(props) {
+  return (
+    <tr>
+      <td>
+        Name
+      </td>
+      <td>
+        Gender
+      </td>
+    </tr>
+  );
+}
+
+function StudentList(props) {
+}
+
+function Retreat(props) {
+}
